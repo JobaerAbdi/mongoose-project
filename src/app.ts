061be654483f6b540277bbb4cb1db66872d53d60
JobaerAbdi <jobaerrahman757@gmail.com>
2023-11-19 10:46:59 +0600

    Final Push

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import { StudentRoutes } from './app/modules/student/student.routes'
 
 const app: Application = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 10
+//    api/v1/students     => get
+//    api/v1/students/create-student  => post
+app.use('/api/v1/students', StudentRoutes)
 
+const getAController = (req: Request, res: Response) => {
+  const a = 10
   res.send(a)
-})
+}
+
+app.get('/', getAController)
 
 export default app
